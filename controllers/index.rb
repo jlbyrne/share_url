cddf1f1f5dbece61427e105020172a79c1a7f924
Jennifer Byrne <jenniferbyrne@Jennifers-MacBook-Pro.local>
2014-04-07 21:12:11 -0700

    finished initial styling

--- controllers/index.rb
+++ controllers/index.rb
@@ -2,6 +2,10 @@ get '/' do
   erb :index
 end
 
+get '/make_a_link' do
+	erb :make_a_link
+end
+
 post '/make_url' do
 	app_id = params[:app_id] != "" ? params[:app_id] : 145634995501895
 	link = params[:link] 
@@ -10,13 +14,13 @@ post '/make_url' do
 	description = url_safe_text(params[:description]) 
 	picture_link = url_safe_link(params[:picture_link]) 
 	#this is where facebook will say the post comes from
-	redirect_uri = params[:redirect_uri] != "" ? params[:redirect_uri] : "https://developers.facebook.com/tools/explorer"
-	#source is a media file url
-	source = params[:source] 
+	redirect_uri = params[:redirect_uri] != "" ? url_safe_link(params[:redirect_uri]) : url_safe_link("https://developers.facebook.com/tools/explorer")
 	#options here are popup and page
 	display = "page"
 
-	p params
+	"https://www.facebook.com/dialog/feed?app_id=#{app_id}&display=#{display}&name=#{name}&caption=#{caption}&link=#{link}&picture=#{picture_link}&description=#{description}&redirect_uri=#{redirect_uri}"
+end
 
-	p url_string = "https://www.facebook.com/dialog/feed?app_id=#{app_id}&display=#{display}&name=#{name}&caption=#{caption}&source=#{source}&link=#{link}&picture=#{picture_link}&description=#{description}&redirect_uri=#{redirect_uri}&show_error=true"
+get '/help' do 
+	erb :help
 end
--- public/application.js
+++ public/application.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
-
+	explainerListener();
+	displayLink();
+	moarOptions();
 });
+
+
+function explainerListener() {
+	$("form").on("focus", "input", function(e){
+		$(this).next().slideDown();
+	});
+
+	$("form").on("focusout", "input", function(e){
+		$(this).next().slideUp();
+	});
+}
+
+function displayLink() {
+	$("form").submit(function(e){
+		e.preventDefault();
+		$.post("/make_url", $("form").serialize(), function(data){
+			$(".link p").html(data);
+			$(".link").slideDown();
+		});
+	});
+}
+
+function moarOptions() {
+	$(".advanced-options-toggle").on("click", function(e){
+		if ($($(this).children()[0]).attr("class") === "arrow-right") {
+			$($(this).children()[0]).removeClass("arrow-right").addClass("arrow-down");
+			$(".advanced-options").slideDown();
+		} else {
+			$($(this).children()[0]).removeClass("arrow-down").addClass("arrow-right");
+			$(".advanced-options").slideUp();
+		}
+	});
+}
