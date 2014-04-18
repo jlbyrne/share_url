99c6b8fffdf864fab02cf4d55bfb3bee990d954b
Jennifer Byrne <jenniferbyrne@Jennifer-Byrnes-MacBook-Pro.local>
2014-04-17 19:43:47 -0700

    put real url in the app

--- controllers/index.rb
+++ controllers/index.rb
@@ -7,19 +7,18 @@ get '/make_a_link' do
 end
 
 post '/make_url' do
-	app_id = params[:app_id] != "" ? params[:app_id] : 145634995501895
+	app_id = params[:app_id] != "" ? params[:app_id] : 756906624327349
 	link = params[:link] 
 	caption = params[:caption] 
 	name = url_safe_text(params[:name]) 
 	description = url_safe_text(params[:description]) 
 	picture_link = url_safe_link(params[:picture_link]) 
 	#this is where facebook will say the post comes from
-	redirect_uri = params[:redirect_uri] != "" ? url_safe_link(params[:redirect_uri]) : url_safe_link("https://developers.facebook.com/tools/explorer")
+	redirect_uri = params[:redirect_uri] != "" ? url_safe_link(params[:redirect_uri]) : url_safe_link("http://www.shareurl.info")
 	#options here are popup and page
 	display = "page"
 
 	"https://www.facebook.com/dialog/feed?app_id=#{app_id}&display=#{display}&name=#{name}&caption=#{caption}&link=#{link}&picture=#{picture_link}&description=#{description}&redirect_uri=#{redirect_uri}"
-	"Coming Soon!"
 end
 
 get '/help' do 
